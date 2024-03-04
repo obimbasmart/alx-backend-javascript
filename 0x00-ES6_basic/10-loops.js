679c157a8eff0f32fea9594e383d79c43fe8a01e
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-04 23:10:20 +0100

    fix: failing check

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // eslint-disable-next-line guard-for-in
-  for (const idx in array) {
-    const value = array[idx];
-    // eslint-disable-next-line no-param-reassign
-    array[idx] = appendString + value;
+  const newArray = [];
+  for (const item of array) {
+    newArray.push(appendString + item);
   }
 
-  return array;
+  return newArray;
 }
