044088dff10df731369e98d232ecdc3fdab6cf7d
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-06 12:25:00 +0100

    fix: add return to func

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()])
+  return Promise
+    .all([createUser(), uploadPhoto()])
     .then((values) => console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
