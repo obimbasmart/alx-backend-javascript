6a2056fcebb9b0ff652469487c634d65549b58c5
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-06 10:34:02 +0100

    fix: add catch handler to handle error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,5 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()])
-    .then((values) => console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`));
+    .then((values) => console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`))
+    .catch(() => console.log('Signup system offline'));
 }
