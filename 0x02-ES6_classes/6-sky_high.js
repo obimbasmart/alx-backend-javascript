ecf2f15d48dd4a7ac335f252f7c6cca07693de68
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-10 20:58:03 +0100

    new: add new task

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+/**
+ * class SkyHighBuilding {extends Building}
+ * @sqft: Number
+ * @floors: Number
+ */
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() { return this._floors; }
+
+  set floors(value) { this._floors = value; }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() { return 'Evacuate slowly the NUMBER_OF_FLOORS floors'; }
+}
+
+export default SkyHighBuilding;
