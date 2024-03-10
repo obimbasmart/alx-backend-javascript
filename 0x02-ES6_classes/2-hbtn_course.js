a13a07a4508ab52f77ca4793c95fb038592233d7
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-10 14:54:05 +0100

    fix: logical error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,16 @@
 class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof (name) !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof (length) !== 'number') {
+      throw TypeError('Length must be a string');
+    }
+
+    if (typeof (students) !== 'object') {
+      throw TypeError('Students must be an iterable');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
