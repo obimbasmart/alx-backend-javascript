8ed370d8de2a754e8f8927303811fd8d48770dfd
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-13 13:19:50 +0100

    fix: validate param type

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,7 +6,7 @@
  * @startString: string
  */
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
 
