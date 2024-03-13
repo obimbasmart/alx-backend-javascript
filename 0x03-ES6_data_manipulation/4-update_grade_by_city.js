84fabd2a1ece08a9dbaf3afa37f9e0b2855962f8
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-13 11:17:33 +0100

    add: new task

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,21 @@
+/**
+ * updateStudentGradeByCity - returns an array of students for a specific city
+ * with their new grade
+ *
+ * @arr: list of students object
+ * @city: string
+ * @newGrades: array of object { studentId: 78, grade: 50 }
+ */
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  const students = arr.filter((student) => student.location === city);
+
+  return students.map((student) => {
+    const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
+
+    if (studentGrade.length) {
+      return { ...student, grade: studentGrade[0].grade };
+    }
+
+    return { ...student, grade: 'N/A' };
+  });
+}
