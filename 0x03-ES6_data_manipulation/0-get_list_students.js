e50ec00fee605ab5f479e4e8c5521b7eaf738857
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-13 10:13:14 +0100

    new: add first task

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+/**
+ * return an array of student object
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
