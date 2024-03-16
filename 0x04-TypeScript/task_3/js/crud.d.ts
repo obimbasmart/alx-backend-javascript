a1d4c8e5ab9b4e33c79e2517138db8361038376f
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-16 11:22:39 +0100

    add: task 4

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,7 @@
+import { RowID, RowElement } from './interface'
+
+export  function insertRow(rowID: rowID): number;
+
+export  function deleteRow(row: RowId): void;
+
+export  function updateRow(rowId: RowID, row: RowElement): number;
